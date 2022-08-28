12fa3b3d3fb99ebe59bb22ca3e2ac149ea78d4ac
dwallace <domenica.wallace5@gmail.com>
2022-08-28 00:36:05 -0400

    added 'new' route

--- server.js
+++ server.js
@@ -6,16 +6,26 @@ const port = 3000
 const party = require('./models/party.js')
 console.log(party)
 
+// app.get('/party', (req, res) => {
+//   res.send(party)
+// })
+
+
 app.get('/party', (req, res) => {
-  res.send(party)
+  res.render('index.ejs', {
+    partyItems: party
+  })
 })
 
+app.get('/party/new' , (req,res) => {
+  res.render('new.ejs')
+})
 
 
 app.get('/party/:idOfPartyObject', (req, res) => {
   res.render('show.ejs', {
     // partyItems: party[req.params.idOfPartyObject]
-    partyItems: party[req.params.idOfPartyObject]
+    partyItem: party[req.params.idOfPartyObject]
   })
 })
 
